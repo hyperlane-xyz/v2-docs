a6cfa1f18dbfe02142d882d3fa0069a3d9605876
Yorke Rhodes <yorke@hyperlane.xyz>
2023-02-23 15:48:43 -0500

    Update sync cmd

--- sync-partials.js
+++ sync-partials.js
@@ -2,7 +2,7 @@ import glob from "glob";
 import fs from "fs";
 
 // use this comment syntax to include markdown partials between the include and end tags
-// <!-- INCLUDE partials/section.md -->
+// <!-- INCLUDE path-to-partial.md -->
 // <!-- END -->
 
 const includeRegex = /(\<\!\-\- INCLUDE (.*.md) -->)(.|\n)*(<!-- END -->)/g;
@@ -11,8 +11,7 @@ const warning = "<!-- WARNING: copied from the included markdown path. Do not ed
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_function_as_the_replacement
 function replacer(_, includeTag, markdownFile, __, endTag) {
   const content = fs.readFileSync(markdownFile, 'utf8');
-  const result = `${includeTag}\n${warning}\n${content}\n${warning}\n${endTag}`;
-  return result;
+  return `${includeTag}\n${warning}\n${content}\n${warning}\n${endTag}`;
 }
 
 function modifyFile(path) {
