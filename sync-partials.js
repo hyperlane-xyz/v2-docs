3922357480432221bc1df026ae194bcfcf4683cf
Yorke Rhodes <yorke@hyperlane.xyz>
2023-02-24 12:35:44 -0500

    Small updates and logs

--- sync-partials.js
+++ sync-partials.js
@@ -10,21 +10,23 @@ const warning =
   "<!-- WARNING: copied from the included file path. Do not edit directly. -->";
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_function_as_the_replacement
-function replacer(match, includeTag, file, __, endTag) {
-  console.log(`${match}\n\n\n`);
-  const body = fs.readFileSync(file, "utf8");
-  let content;
-  if (file.endsWith(".md")) {
-    content = body;
-  } else if (file.endsWith(".sol")) {
-    content = `\`\`\`solidity\n${body}\n\`\`\``;
-  }
-  return `${includeTag}\n${warning}\n${content}\n${warning}\n${endTag}`;
+function replacer(path) {
+  return (_, includeTag, file, __, endTag) => {
+    console.log(`${path} << ${file}`)
+    const body = fs.readFileSync(file, "utf8");
+    let content;
+    if (file.endsWith(".md")) {
+      content = body;
+    } else if (file.endsWith(".sol")) {
+      content = `\`\`\`solidity\n${body}\n\`\`\``;
+    }
+    return [includeTag, warning, content, warning, endTag].join("\n");
+  };
 }
 
 function modifyFile(path) {
   let file = fs.readFileSync(path, "utf8");
-  file = file.replace(includeRegex, replacer);
+  file = file.replace(includeRegex, replacer(path));
   fs.writeFileSync(path, file);
 }
 
